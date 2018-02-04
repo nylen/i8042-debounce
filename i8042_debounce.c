28c1c01899f2965cff23ca9c99fa17e622c66919
ivanbrennan <ivan.brennan@gmail.com>
2018-02-04 17:48:42 -0500

    adjust thresholds: 90, 75
    This feels pretty good. It catches nearly all the bogus keybounces, and
    the only intentional key presses it inhibits are tense, single-finger
    repeats (e.g. `cd ..`). I don't mind relaxing those
    movements a bit.

--- i8042_debounce.c
+++ i8042_debounce.c
@@ -76,14 +76,14 @@ static bool i8042_debounce_filter(
 			key->is_down = true;
 			keys_currently_down++;
 		}
-		if (unlikely(keys_currently_down > 1 && msecs_since_keyup < 55)) {
+		if (unlikely(keys_currently_down > 1 && msecs_since_keyup < 90)) {
 			pr_info(
 				"i8042_debounce key=%02x press blocked multi ms=%u\n",
 				data, msecs_since_keyup
 			);
 			key->block_next_keyup = true;
 			return true;
-		} else if (unlikely(msecs_since_keyup < 40)) {
+		} else if (unlikely(msecs_since_keyup < 75)) {
 			pr_info(
 				"i8042_debounce key=%02x press blocked single ms=%u\n",
 				data, msecs_since_keyup
